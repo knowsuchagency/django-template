a31b95801de71be3c07edbb6663faaecd4fea4e9
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2023-12-31 19:05:49 -0800

    use urlsplit instead of urlparse

--- {{project_name}}/core/middlewares.py
+++ {{project_name}}/core/middlewares.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlparse
+from urllib.parse import urlsplit
 from django.conf import settings
 
 
@@ -6,7 +6,7 @@ def htmx_redirect_middleware(get_response):
     def middleware(request):
         response = get_response(request)
         if request.headers.get("HX-Request") and response.status_code == 302:
-            location = urlparse(response["Location"])
+            location = urlsplit(response["Location"])
             if location.path == settings.LOGIN_URL:
                 redirect_url = (
                     f"{settings.LOGIN_URL}?next={settings.LOGIN_REDIRECT_URL}"

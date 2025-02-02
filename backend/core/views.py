c0f86c1e549da828c7117646235714ade33bea31
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2025-02-01 16:42:42 -0800

    Add endpoint to set CSRF token for frontend

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+
+@ensure_csrf_cookie
+def set_csrf_token(request):
+    return JsonResponse({"details": "CSRF cookie set"})
 
 
 def landing_page(request):

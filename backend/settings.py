648ced56b560d42c9155341b595ad236388fba95
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2025-02-01 16:18:32 -0800

    Add CSRF trusted origins for local development

--- backend/settings.py
+++ backend/settings.py
@@ -52,6 +52,12 @@ if DEBUG:
     ALLOWED_HOSTS += ["*"]
     CSRF_COOKIE_DOMAIN = None
     SESSION_COOKIE_DOMAIN = None
+    CSRF_TRUSTED_ORIGINS = [
+        "http://localhost:8000",
+        "http://localhost:8080",
+        "http://127.0.0.1:8000",
+        "http://127.0.0.1:8080",
+    ]
 
 INTERNAL_IPS = [
     "127.0.0.1",

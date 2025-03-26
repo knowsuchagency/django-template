6a4f19237b70b07dfc27258e3fbf337a63284c3e
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2025-03-25 20:07:15 -0700

    Remove django-unfold dependency from pyproject.toml, uv.lock, settings.py, and admin.py to streamline project dependencies.

--- src/settings.py
+++ src/settings.py
@@ -162,7 +162,6 @@ AUTHENTICATION_BACKENDS = [
 # Application definition
 
 INSTALLED_APPS = [
-    "unfold",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
